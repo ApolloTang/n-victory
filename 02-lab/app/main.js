f8d8727e99b75b94fb9a87a564c5a8765693a489
Apollo Tang <tang.apollo@gmail.com>
2016-04-02 18:34:10 -0400

    playing with victory-bar and victory-axis

--- 02-lab/app/main.js
+++ 02-lab/app/main.js
@@ -5,5 +5,8 @@ import './main.less';
 // import LabTemplate from './00-lab-template/';
 // render( <LabTemplate />, document.getElementById('root'));
 
-import SimpleBar from './01-simple-bar/';
-render( <SimpleBar />, document.getElementById('root'));
+// import SimpleBar from './01-simple-bar/';
+// render( <SimpleBar />, document.getElementById('root'));
+
+import Comp from './02-axis/';
+render( <Comp />, document.getElementById('root'));
